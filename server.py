0226129801fae7e0161865557453592768b3ed22
keerthi vasan <keerthivasan676@gmail.com>
2022-12-16 16:58:24 +0530

    deploy

--- server.py
+++ server.py
@@ -1,29 +1,25 @@
 from fastapi import FastAPI,Request
+from fastapi.responses import JSONResponse
 import uvicorn
 import pandas as pd
 import numpy as np
 from main import *
+from schema import *
 
 app = FastAPI()
-
+recommendation = SpotifyRecommendation(model_data)
 @app.get('/')
 def home():
     return {"status":"Server is Running"}
 
-#@app.post('/recommend')
+@app.post('/Recommendation_system')
 
-# async def song_rec(x: input(), y: input(), request: Request):
-#     result = await request.json()
-#     recommendation = SpotifyRecommendation(model_data)
+async def song_recommder(x:SongRecommendation, request: Request):
+    result = await request.json()
+    rec = recommendation.recommend(result['song_name'],result['number_of_songs'])
+    song = dict(rec['track_name'])
 
-#     rec = recommendation.recommend(x,y)
-
-#     return {rec}
+    return song
 
 if __name__=="__main__":
     uvicorn.run('server:app',port=1234,reload=True)
-
-
-
-
-
